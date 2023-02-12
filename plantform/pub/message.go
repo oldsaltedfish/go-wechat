5cb796ab1d1abd6d40c3a193e35a33691072d19d
anoldsaltedfish <rmzf@qq.com>
2023-02-12 15:53:07 +0800

    message

--- plantform/pub/message.go
+++ plantform/pub/message.go
@@ -1,34 +1,55 @@
 package pub
 
+type ReceiveMsgType string
+
+type ReplyMsgType string
+
+const (
+	ReceiveMsgTypeText       ReceiveMsgType = "text"
+	ReceiveMsgTypeImage      ReceiveMsgType = "image"
+	ReceiveMsgTypeVoice      ReceiveMsgType = "voice"
+	ReceiveMsgTypeVideo      ReceiveMsgType = "video"
+	ReceiveMsgTypeShortVideo ReceiveMsgType = "shortvideo"
+	ReceiveMsgTypeLocation   ReceiveMsgType = "location"
+	ReceiveMsgTypeLink       ReceiveMsgType = "link"
+
+	ReplyMsgTypeText  ReplyMsgType = "text"
+	ReplyMsgTypeImage ReplyMsgType = "image"
+	ReplyMsgTypeVoice ReplyMsgType = "voice"
+	ReplyMsgTypeVideo ReplyMsgType = "video"
+	ReplyMsgTypeMusic ReplyMsgType = "music"
+	ReplyMsgTypeNews  ReplyMsgType = "news"
+)
+
 type CDATA struct {
 	Text string `xml:",cdata"`
 }
 
 type ReceiveMsg struct {
-	ToUserName   string `xml:"ToUserName"`
-	FromUserName string `xml:"FromUserName"`
-	CreateTime   uint64 `xml:"CreateTime"`
-	MsgType      string `xml:"MsgType"`
-	MsgId        uint64 `xml:"MsgId"`
-	MsgDataId    string `xml:"MsgDataId"`
-	Idx          string `xml:"Idx"`
-	Content      string `xml:"Content"`
-	PicUrl       string `xml:"PicUrl"`
-	MediaId      string `xml:"MediaId"`
-	Format       string `xml:"Format"`
-	ThumbMediaId string `xml:"ThumbMediaId"`
-	Description  string `xml:"Description"`
-	Url          string `xml:"Url"`
-	LocationX    string `xml:"Location_X"`
-	LocationY    string `xml:"Location_Y"`
-	Label        string `xml:"Label"`
+	ToUserName   string         `xml:"ToUserName"`
+	FromUserName string         `xml:"FromUserName"`
+	CreateTime   uint64         `xml:"CreateTime"`
+	MsgType      ReceiveMsgType `xml:"MsgType"`
+	MsgId        uint64         `xml:"MsgId"`
+	MsgDataId    string         `xml:"MsgDataId"`
+	Idx          string         `xml:"Idx"`
+	Content      string         `xml:"Content"`
+	PicUrl       string         `xml:"PicUrl"`
+	MediaId      string         `xml:"MediaId"`
+	Format       string         `xml:"Format"`
+	ThumbMediaId string         `xml:"ThumbMediaId"`
+	Description  string         `xml:"Description"`
+	Url          string         `xml:"Url"`
+	LocationX    string         `xml:"Location_X"`
+	LocationY    string         `xml:"Location_Y"`
+	Label        string         `xml:"Label"`
 }
 
 type MediaId struct {
 	MediaId CDATA `xml:"MediaId"`
 }
 
-type SendMsg struct {
+type ReplyMsg struct {
 	ToUserName   CDATA     `xml:"ToUserName"`
 	FromUserName CDATA     `xml:"FromUserName"`
 	CreateTime   uint64    `xml:"CreateTime"`
